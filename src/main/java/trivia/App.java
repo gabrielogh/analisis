94eb3bab84ba9656ebc186357c9e0aa00ff5b15e
Gabriel <gabriel@MacBook-Pro-de-Gabriel.local>
2017-06-09 14:27:32 -0300

    Fixes: Users validatios works- Users Register works- Users login works.

--- src/main/java/trivia/App.java
+++ src/main/java/trivia/App.java
@@ -26,7 +26,7 @@ public class App{
 
 		
     staticFiles.location("/public");
-
+    /*
  	  //Creamos una categoria, una pregunta y sus respuesta a modo de poder testear le game.
     Category c = new Category("Historia");
     c.saveIt();
@@ -35,12 +35,35 @@ public class App{
     q.saveIt();
     String[] catResult = {(String)q.get("description"), (String)q.get("a1"), (String)q.get("a2"), (String)q.get("a3"), (String)q.get("a4")};
     
+     	  //Creamos una categoria, una pregunta y sus respuesta a modo de poder testear le game.
+    Category ciencia = new Category("Ciencia");
+    ciencia.saveIt();
+    Question qciencia = new Question("Cual de los siguientes numeros binarios representa el 10 decimal?", "0000010", "0001010", "11111111", "0001000", 2);
+    qciencia.set("category_id", ciencia.get("id"));
+    qciencia.saveIt();
+    
+     	  //Creamos una categoria, una pregunta y sus respuesta a modo de poder testear le game.
+    Category ent = new Category("Entrenenimiento");
+    ent.saveIt();
+    Question qent = new Question("Como le dicen a Macri?", "Pato", "Raro", "Malo", "Gatooooooo", 4);
+    qent.set("category_id", ent.get("id"));
+    qent.saveIt();
+    
+     	  //Creamos una categoria, una pregunta y sus respuesta a modo de poder testear le game.
+    Category cdep = new Category("Deporte");
+    cdep.saveIt();
+    Question qdep = new Question("Por cuantos goles le ganaria River a Boca si jugaran hoy?", "1-0", "2-0", "4-0", "Boca no asistiria", 4);
+    qdep.set("category_id", cdep.get("id"));
+    qdep.saveIt();
+    
+    Question q= new
     Map quest = new HashMap();
     quest.put("desc", catResult[0]);
     quest.put("a1", catResult[1]);
     quest.put("a2", catResult[2]);
     quest.put("a3", catResult[3]);
     quest.put("a4", catResult[4]);
+    */
 
     //Mensaje de Bienvenida
     Map map = new HashMap();
@@ -79,14 +102,19 @@ public class App{
     	req.session(true);
     	String password = req.queryParams("password");
       String namee = req.queryParams("username");
-      System.out.println(namee);
-      req.session().attribute("username", namee);
-      req.session().attribute("password", password);
-      Map resLogin = new HashMap();
-    	if((namee!=null) && (password != null)){
+      //Verificamos si existe algun usuario con ese username y esa pass.
+	    List<User> unico = User.where("username = ? and password = ?", namee,password);
+	    Boolean result2 = unico.size()==0;
+			Map resLogin = new HashMap();
+			if(!result2){
+	      req.session().attribute("username", namee);
+	      req.session().attribute("password", password);
     		resLogin.put("username",namee);
     		resLogin.put("play", "Jugar");
-    	}
+	    }
+	    else{
+	    	System.out.println("Usuario incorrecto");
+	    }
     	Base.close();
     	return new ModelAndView(resLogin,"./views/index.html");   	
     }, new MustacheTemplateEngine()
@@ -99,24 +127,31 @@ public class App{
   	// Se cargan los parámetros de la query (URL) en un arreglo
     String[] result = {req.queryParams("username"),req.queryParams("email"),req.queryParams("password")};
   	String body = req.body();
-  	User u = new User(result[0],result[1], result[2]);
-  	u.saveIt();
-    Category cat = (new Category()).randomCat();
-    Question que = cat.getQuestion();
-    String[] resQ = {(String)que.get("description"),(String)que.get("a1"),(String)que.get("a2"),(String)que.get("a3"),(String)que.get("a4")};
-    Map questt = new HashMap();
-    questt.put("usernamee", result[0]);
-    questt.put("desc", resQ[0]);
-    questt.put("a1", resQ[1]);
-    questt.put("a2", resQ[2]);
-    questt.put("a3", resQ[3]);
-    questt.put("a4", resQ[4]);
-    questt.put("play", "play");
-    questt.put("Usuario", "Usuario");
-    questt.put("id", u.getId());
-
+  	Map questt = new HashMap();
+    List<User> unico = User.where("username = ? or mail = ? ", result[0], result[1]);
+    Boolean result2 = unico.size()==0;
+  	if(result2){
+	  	User u = new User(result[0],result[1], result[2]);
+	  	u.saveIt();
+	    Category cat = (new Category()).randomCat();
+	    Question que = cat.getQuestion();
+	    String[] resQ = {(String)que.get("description"),(String)que.get("a1"),(String)que.get("a2"),(String)que.get("a3"),(String)que.get("a4")};
+	    questt.put("categ", cat.get("name"));
+	    questt.put("usernamee", result[0]);
+	    questt.put("desc", resQ[0]);
+	    questt.put("a1", resQ[1]);
+	    questt.put("a2", resQ[2]);
+	    questt.put("a3", resQ[3]);
+	    questt.put("a4", resQ[4]);
+	    questt.put("play", "play");
+	    questt.put("Usuario", "Usuario");
+	    questt.put("id", u.getId());
+	  }
+	  else{
+	  	System.out.println("El usuario ya existe");
+	  }
   	Base.close();
-    return new ModelAndView(questt, "./views/index.html");
+    return new ModelAndView(questt, "./views/play.html");
   	}, new MustacheTemplateEngine());
 		
 
